2ca4789c5b0274cc5a6f074cd5fe5cd0b649b271
joaovittordjesus <joaovittordomingosdejesus@outlook.com>
2024-03-04 16:07:56 -0300

    SpringBoot: Creating a prject and printing data

---
+++ helloworld/src/main/java/webapp/helloworld/HelloworldApplication.java
@@ -0,0 +1,14 @@
+package webapp.helloworld;
+
+// import webapp.helloworld.IndexController;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloworldApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloworldApplication.class, args);
+	}
+
+}
