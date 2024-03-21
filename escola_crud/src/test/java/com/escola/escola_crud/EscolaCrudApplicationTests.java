6e2848dddf4875c15aa5edca4d4abc24ff0439e1
joaovittordjesus <joaovittordomingosdejesus@outlook.com>
2024-03-21 15:57:37 -0300

    Criando repositório raiz para projetos SpringBoot

---
+++ escola_crud/src/test/java/com/escola/escola_crud/EscolaCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.escola.escola_crud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EscolaCrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
